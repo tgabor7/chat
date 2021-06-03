b97a97c2b3f4408f66b22e23e84dd4f9cf552b32
tgabor7 <tgabor7@gmail.com>
2021-06-03 19:11:47 +0200

    Style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import logo from './logo.svg';
+import icon from './icon.svg';
 import './App.css';
-import {useState} from 'react'
+import React, {useState} from 'react'
 
 const axios = require('axios')
 
 function App() {
 
-  var [message, setMessage] = useState('asd\nqwe\nasd')
-  
+  var [messages, setMessages] = useState([{message: 'asd', sender: 0},{message: 'qwe', sender: 1}])
+  var [message, setMessage] = useState('')
+
+  const name = "Fuck face";
+  let sendButton = React.createRef();
+  let messageField = React.createRef();
+  let container = React.createRef();
+ 
+  const messages_render = messages.map(e=>{
+	  return <div><div className={e.sender == 0 ? "message0" : "message1"}>{e.message} </div><br /><br /></div>
+  })
+
+  let updateMessages = (m)=>{
+	let currentMessages = messages.slice();
+	currentMessages.push({message: m, sender: 0})
+	setMessages(currentMessages)
+  }
+  let validateMessage = (m)=>{
+	  if(m.length < 1) return false
+	  return true
+  }
+
+  updateMessages.bind(this)
+  validateMessage.bind(this)
+
   return ( <div className="App">
-	{message} <br/>
-	  <input type="text" onChange={e=>{setMessage(e.target.value)}}></input>
-	  <input type="button" onClick={()=>{
-		  axios.get('http://localhost:4000/chat/get', {
-			header: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
+	<div className="container" ref={container}>
+	<div className="name">{name}</div>
+
+	<div className="messageContainer">
+	{messages_render}
+	</div>
+	
+	</div>
+	<br/>
+	<div className="inputContainer">
+	  <input type="text" class="messageField" ref={messageField} 
+	 onFocus={e=>{
+		 
+	 }} 
+	  onKeyPress={e=>{if(e.key === 'Enter') sendButton.current.click()}}  onChange={
+		  e=>{
+			  setMessage(e.target.value)
 			}
-		  }).then(response=>{
-			  alert(response.data)
-		  }).catch(err=>{console.log(err)})
-	  }} value="Send"></input>
+		  }></input>
+	  <input type="button" ref={sendButton} className="sendButton" onClick={e=>{
+		  if(!validateMessage(message)) return
+		  updateMessages(message)
+		  messageField.current.value = '';
+		  setMessage('')
+		  let c = container.current
+		  let scroll = c.offsetHeight
+
+		  setTimeout(function(){
+			c.scroll(0, scroll)
+			scroll+=100
+		  },100)
+		  }} value="Send"></input></div>
    </div>);
 }
 
